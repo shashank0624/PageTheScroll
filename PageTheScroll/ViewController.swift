87db5c3ac663fd1f1a12c6a2d8e15b127e09d608
Shashank Panwar <shashankpanwar4@gmail.com>
2017-12-04 17:29:24 +0530

    Add scroll view
    When the scroll view width is same as the base view width,,

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var images = [UIImageView]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        var contentWidth: CGFloat = 0.0
+        for x in 0...2{
+            let image = UIImage(named: "icon\(x)")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            scrollView.addSubview(imageView)
+            
+            var newX : CGFloat = 0.0
+            newX = self.view.frame.midX + self.view.frame.size.width * CGFloat(x)
+            
+            contentWidth += newX
+            print("Content Size: \(contentWidth)")
+            
+            imageView.frame = CGRect(x: newX - 75, y: (self.view.frame.size.height / 2) - 75, width: 150, height: 150)
+        }
+        scrollView.contentSize = CGSize(width: self.view.frame.size.width * CGFloat(images.count), height: self.view.frame.size.height)
+        
     }
-
 
 }
 
