9aebc2ee545f047bfe7f7e62e58a7fcb398fd290
Shashank Panwar <shashankpanwar4@gmail.com>
2017-12-07 18:28:52 +0530

    Add a custom scroll View

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -19,24 +19,27 @@ class ViewController: UIViewController {
         
     }
     
-    override func viewWillAppear(_ animated: Bool) {
+    override func viewDidAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         var contentWidth: CGFloat = 0.0
+        let scrollWidth = scrollView.frame.size.width
         for x in 0...2{
             let image = UIImage(named: "icon\(x)")
             let imageView = UIImageView(image: image)
             images.append(imageView)
-            scrollView.addSubview(imageView)
             
             var newX : CGFloat = 0.0
-            newX = self.view.frame.midX + self.view.frame.size.width * CGFloat(x)
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
             
             contentWidth += newX
             print("Content Size: \(contentWidth)")
+            scrollView.addSubview(imageView)
             
-            imageView.frame = CGRect(x: newX - 75, y: (self.view.frame.size.height / 2) - 75, width: 150, height: 150)
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
         }
-        scrollView.contentSize = CGSize(width: self.view.frame.size.width * CGFloat(images.count), height: self.view.frame.size.height)
+    
+        scrollView.clipsToBounds = false
+        scrollView.contentSize = CGSize(width: scrollView .frame.size.width * CGFloat(images.count), height: self.view.frame.size.height)
         
     }
 
